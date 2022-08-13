f65a766516110a08ac4457c62418d41f0e4b686b
Nahuel Carrizo <nhcarrizo@gmail.com>
2022-08-13 10:48:57 -0300

    pug

--- dist/clase10/pug/server.js
+++ dist/clase10/pug/server.js
@@ -1,47 +1,35 @@
 const express = require("express");
-const { Router } = express;
 const Producto = require("../Producto");
 const app = express();
+const { Router } = express;
 const PORT = 4000 || process.env.PORT;
 const routerProductos = Router();
 const productos = new Producto("../productos.txt");
 
-const handlebars = require("express-handlebars");
+const pug = require("pug");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/static", express.static(__dirname + "/public"));
 app.use("/api/productos", routerProductos);
 
-app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: "hbs",
-    defaultLayout: "index.hbs",
-    layoutsDir: __dirname + "/views/layouts",
-    partialsDir: __dirname + "/views/partials",
-  })
-);
-
-app.set("view engine", "hbs");
 app.set("views", "./views");
+app.set("view engine", "pug");
 
 routerProductos.get("/", (req, res) => {
-  res.render("main");
+  res.render("index");
 });
 
 routerProductos.get("/listaproductos/", async (req, res) => {
   const prods = await productos.getAll();
-  console.log(prods);
   res.render("productos", { prods: prods });
 });
 
 routerProductos.post("/", async (req, res) => {
   productos.save(req.body);
-  res.status(200).render("main");
+  res.status(200).render("index");
 });
 
-app.listen(PORT, (err) => {
-  if (err) throw new Error(`Error on server: ${err}`);
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
