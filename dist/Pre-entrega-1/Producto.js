0f5abe24ad226c77f7e9421092b4559603b6946a
Nahuel Carrizo <nhcarrizo@gmail.com>
2022-08-29 16:33:58 -0300

    [server]

---
+++ dist/Pre-entrega-1/Producto.js
@@ -0,0 +1,121 @@
+const fs = require("fs");
+
+class Producto {
+  constructor(url) {
+    this.url = url;
+  }
+  async save(obj) {
+    try {
+      console.log(obj);
+      let data = await fs.promises.readFile(this.url, "utf-8");
+      let dataParse = JSON.parse(data);
+      const timeStamp = Date.now();
+      if (dataParse.length) {
+        await fs.promises.writeFile(
+          this.url,
+          JSON.stringify(
+            [
+              ...dataParse,
+              {
+                ...obj,
+                id: dataParse[dataParse.length - 1].id + 1,
+                timestamp: timeStamp,
+              },
+            ],
+            null,
+            2
+          )
+        );
+        return "cargado";
+      } else {
+        await fs.promises.writeFile(
+          this.url,
+          JSON.stringify([{ ...obj, id: 1, timestamp: timeStamp }], null, 2)
+        );
+      }
+      return dataParse;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async getById(id) {
+    try {
+      const data = await fs.promises.readFile("./productos.txt", "utf-8");
+      const dataParse = JSON.parse(data);
+      console.log(dataParse);
+      console.log(id);
+      const product = dataParse.find((obj) => obj.id == id);
+      if (product) {
+        return product;
+      }
+    } catch (error) {}
+  }
+
+  async getAll() {
+    try {
+      const data = await fs.promises.readFile(this.url, "utf-8");
+      const dataParse = await JSON.parse(data);
+      if (dataParse.length) {
+        return dataParse;
+      } else {
+        console.log("no hay producto");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deletById(req, res) {
+    try {
+      const id = Number(req.params.id);
+      const data = await fs.promises.readFile(
+        this.url,
+        "utf-8",
+        function (err, data) {
+          if (err) throw err;
+        }
+      );
+      const dataParse = JSON.parse(data);
+      let product = dataParse.find((obj) => obj.id === id);
+      if (typeof product === "undefined") {
+        res.status(404).json({ error: "Producto no encontrado" });
+      } else {
+        dataParse.splice(dataParse.indexOf(product), 1);
+        fs.writeFile(this.url, JSON.stringify(dataParse), function (err) {
+          if (err) throw err;
+        });
+        res.status(200).json(`Item with id ${id} was deleted`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deleteAll() {
+    await fs.promises.writeFile(this.url, JSON.stringify([], null, 2), "utf-8");
+  }
+
+  async updateById(obj) {
+    try {
+      const data = await fs.promises.readFile(this.url, "utf-8");
+      const dataParse = JSON.parse(data);
+
+      const objIndex = dataParse.findIndex((prod) => prod.id === obj.id);
+      if (objIndex !== -1) {
+        dataParse[objIndex] = obj; // => array[1] -> {}
+        await fs.promises.writeFile(
+          this.url,
+          JSON.stringify(dataParse, null, 2)
+        );
+        console.log("actualizado");
+        return { msg: "actualizado el producto" };
+      } else {
+        return { error: "no existe el producto" };
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+module.exports = Producto;
---
+++ dist/Pre-entrega-1/server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const { Router } = express;
+const PORT = process.env.PORT || 8080;
+const routerProductos = Router();
+const routerCarrito = Router();
+const Producto = require("./Producto");
+const productos = new Producto("./productos.txt");
+const Carrito = require("./Carrito");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api/productos", routerProductos);
+app.use("/api/carrito", routerCarrito);
+
+app.set("view engine", "ejs");
+app.set("views", "./public/views/layouts");
+
+let admin = true;
+
+//////////////////PRODUCTOS
+routerProductos.get("/", (req, res) => {
+  res.render("index");
+});
+
+routerProductos.get("/listaproductos/", async (req, res) => {
+  const prods = await productos.getAll();
+  res.render("products", { prods: prods });
+});
+
+routerProductos.post("/listaproductos/", async (req, res) => {
+  const timeStamp = Date.now();
+  const carrito = new Carrito("./carrito.txt", timeStamp);
+  carrito.save(req.body);
+});
+
+routerProductos.post("/", async (req, res) => {
+  if (admin) {
+    productos.save(req.body);
+    res.status(200).render("index");
+  } else {
+    res.json({ error: -2, descripcion: "ruta 'x' método 'y' no implementada" });
+  }
+});
+
+//////////////////CARRITO
+routerCarrito.get("/", async (req, res) => {
+  try {
+    const carrito = new Carrito("./carrito.txt");
+    const prods = await carrito.getAll();
+    res.render("cart", { prods: prods });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+routerCarrito.post("/", async (req, res) => {
+  try {
+    console.log("hola");
+    res.status(200).render("/layouts/cart");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
